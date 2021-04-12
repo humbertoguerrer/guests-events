f35b0310d3f63ceacdd7c857e4f159d53fb1f41e
humbertoguerrer <humberto.guerrer@gmail.com>
2021-04-12 20:28:13 -0300

    Projeto criado

---
+++ src/main/java/com/hgn/guestsevents/GuestsEventsApplication.java
@@ -0,0 +1,13 @@
+package com.hgn.guestsevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestsEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestsEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hgn/guestsevents/GuestsEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgn.guestsevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestsEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
